a1f9a100d45e008c2bac622f04c152fe4b6322f9
TzuWei <ap9940506@gmail.com>
2022-09-19 12:20:39 +0800

    feat(hw1): redraw chart on changing axis, only plot g & title completed

--- hw1_scatter_plots/311553005.js
+++ hw1_scatter_plots/311553005.js
@@ -1,25 +1,29 @@
 const DATA_PATH = "./iris.csv";
 
-function render(data, xAttr, yAttr) {
+function render(data, { xAttr, yAttr }) {
   if (!data) return;
-  const width = 1000;
+  const width = 600;
   const height = 600;
-  const margin = { left: 100, right: 20, top: 50, bottom: 50 };
+  const margin = { left: 50, right: 50, top: 50, bottom: 50 };
   const innerWidth = width - margin.left - margin.right;
   const innerHeight = height - margin.top - margin.bottom;
   const palette = ["#003f5c", "#bc5090", "#ffa600"];
 
-  const plot = d3.select("svg").attr("width", width).attr("height", height);
+  const plotContainer = d3.select("svg").attr("width", width).attr("height", height);
 
   const xValue = (d) => d[xAttr];
   const yValue = (d) => d[yAttr];
   const xScale = d3
     .scaleLinear()
-    .domain(d3.extent(data.map(xValue)))
+    .domain([0, d3.max(data.map(xValue))])
+    .nice()
+    // .domain(d3.extent(data.map(xValue)))
     .range([0, innerWidth]);
   const yScale = d3
     .scaleLinear()
-    .domain(d3.extent(data.map(yValue)))
+    .domain([d3.max(data.map(yValue)), 0])
+    .nice()
+    // .domain(d3.extent(data.map(yValue)))
     .range([0, innerHeight]);
   const cScale = d3
     .scaleOrdinal()
@@ -29,31 +33,45 @@ function render(data, xAttr, yAttr) {
   const xAxis = d3.axisBottom(xScale);
   const yAxis = d3.axisLeft(yScale);
 
-  const innerPlot = plot.append("g").attr("transform", `translate(${margin.left}, ${margin.top})`);
+  plotContainer
+    .selectAll("g#plot")
+    .data([null])
+    .enter()
+    .append("g")
+    .attr("id", "plot")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  const plot = plotContainer.select("g#plot");
 
   // draw TITLE
-  innerPlot
+  plot
+    .selectAll("text#title")
+    .data([null])
+    .enter()
     .append("text")
+    .attr("id", "title")
     .attr("x", innerWidth / 2)
     .attr("y", -20)
     .attr("text-anchor", "middle")
     .text("Iris distribution");
 
   // draw X AXIS & LABEL
-  innerPlot.append("g").call(xAxis).attr("transform", `translate(0, ${innerHeight})`);
-  innerPlot
+  plot.append("g").call(xAxis).attr("transform", `translate(0, ${innerHeight})`);
+  plot
     .append("text")
-    .attr("x", innerWidth)
-    .attr("y", innerHeight + 40)
-    .attr("text-anchor", "end")
+    .attr("text-anchor", "middle")
+    .attr("transform", `translate(${innerWidth / 2}, ${innerHeight + 40})`)
     .text(xAttr);
 
   // draw Y AXIS & LABEL
-  innerPlot.append("g").call(yAxis);
-  innerPlot.append("text").attr("x", 0).attr("y", -12).attr("text-anchor", "end").text(yAttr);
+  plot.append("g").call(yAxis);
+  plot
+    .append("text")
+    .attr("text-anchor", "middle")
+    .attr("transform", `translate(${-40}, ${innerHeight / 2}) rotate(270)`)
+    .text(yAttr);
 
   // draw circles, DATA POINT
-  innerPlot
+  plot
     .selectAll("circle")
     .data(data)
     .enter()
@@ -66,9 +84,19 @@ function render(data, xAttr, yAttr) {
 
 async function main() {
   const dataset = await d3.csv(DATA_PATH);
-  // const xAttrSelect = document.getElementById("xAttrSelect");
-  // xAttrSelect.addEventListener("onchange", () => {});
-  render(dataset, "sepal length", "sepal width");
+  const axisAttrs = {
+    xAttr: "sepal length",
+    yAttr: "sepal width",
+  };
+  render(dataset, axisAttrs);
+  d3.select("#xAttrSelect").on("change", (event) => {
+    axisAttrs.xAttr = event.target.value;
+    render(dataset, axisAttrs);
+  });
+  d3.select("#yAttrSelect").on("change", (event) => {
+    axisAttrs.yAttr = event.target.value;
+    render(dataset, axisAttrs);
+  });
 }
 
 main();
