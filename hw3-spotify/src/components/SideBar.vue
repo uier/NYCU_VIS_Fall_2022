6c6bd9011a2010075c5311ff2790d795a36b97c4
TzuWei <ap9940506@gmail.com>
2022-11-21 10:48:01 +0800

    hw3 init

---
+++ hw3-spotify/src/components/SideBar.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { defineProps } from "vue";
+
+const { genres } = defineProps(["genres"]);
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <div v-for="genre in genres" :key="genre" class="border my-2">
+      <span>{{ genre }}</span>
+    </div>
+  </div>
+</template>
---
+++ hw3-spotify/src/components/TopBar.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { defineProps } from "vue";
+
+const { stats } = defineProps(["stats"]);
+</script>
+
+<template>
+  <div class="flex h-24 items-center">
+    <div class="py-2 pl-6 flex justify-center items-center h-full">
+      <span class="text-3xl font-bold">Spotify Tracks</span>
+    </div>
+    <div class="flex-1 stats shadow">
+      <div class="stat place-items-center">
+        <div class="stat-title">Tracks</div>
+        <div class="stat-value">{{ stats.numTracks }}</div>
+      </div>
+
+      <div class="stat place-items-center">
+        <div class="stat-title">Genre</div>
+        <div class="stat-value">{{ stats.numGenres }}</div>
+      </div>
+
+      <div class="stat place-items-center">
+        <div class="stat-title">Artists</div>
+        <div class="stat-value">{{ stats.numArtists }}</div>
+      </div>
+
+      <div class="stat place-items-center">
+        <div class="stat-title">Album</div>
+        <div class="stat-value">{{ stats.numAlbums }}</div>
+      </div>
+    </div>
+  </div>
+</template>
